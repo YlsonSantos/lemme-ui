8088fa800698cd206c65ebcf3c0188b0e742bee6
ArthurLima05 <arthurralima2005@gmail.com>
2025-05-08 00:02:26 -0300

    Criando o Menu de Ferramentas e o componente de escolha de cor

--- src/pages/customization/CustomizationPage.js
+++ src/pages/customization/CustomizationPage.js
@@ -4,11 +4,15 @@ import FontPicker from '../../components/FontPicker';
 import { saveTheme } from '../../utils/api';
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
+import ToolMenu from '../../components/ToolMenu';
+<ToolMenu />
+
 
 const CustomizationPage = () => {
     return (
         <div>
             <Header />
+            <ToolMenu />
             <Footer />
         </div>
 
