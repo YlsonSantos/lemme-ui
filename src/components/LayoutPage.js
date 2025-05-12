8c4d4f1dce885b63b6898d933e5592e467459d82
Ylson Santos <ylsonqueiroz@gmail.com>
2025-05-12 13:48:41 -0300

    Salvando alterações antes de mudar para Ylson-branch

--- src/components/LayoutPage.js
+++ src/components/LayoutPage.js
@@ -4,17 +4,23 @@ import '../../public/styles/LayoutPage.css';
 
 const LayoutPage = () => {
   const [mostrarModal, setMostrarModal] = useState(false);
-  const [mostrarSubModal, setMostrarSubModal] = useState(false);
+  const [mostrarSubModalColunas, setMostrarSubModalColunas] = useState(false);
+  const [mostrarSubModalHorizontal, setMostrarSubModalHorizontal] = useState(false);
+  const [mostrarSubModalMisto, setMostrarSubModalMisto] = useState(false);
   const [layoutSelecionado, setLayoutSelecionado] = useState(null);
 
   const abrirModal = () => setMostrarModal(true);
   const fecharModal = () => {
     setMostrarModal(false);
-    setMostrarSubModal(false);
+    setMostrarSubModalColunas(false);
+    setMostrarSubModalHorizontal(false);
+    setMostrarSubModalMisto(false);
   };
 
   const voltarAoModalAnterior = () => {
-    setMostrarSubModal(false);
+    setMostrarSubModalColunas(false);
+    setMostrarSubModalHorizontal(false);
+    setMostrarSubModalMisto(false);
   };
 
   const handleOverlayClick = (e) => {
@@ -42,40 +48,41 @@ const LayoutPage = () => {
         {mostrarModal && (
           <div className="modal-overlay" onClick={handleOverlayClick}>
             <div className="modal-conteudo">
-              <h3 className="modal-titulo">{mostrarSubModal ? 'Colunas' : 'Layout da Página'}</h3>
+              <h3 className="modal-titulo">
+                {mostrarSubModalColunas
+                  ? 'Colunas'
+                  : mostrarSubModalHorizontal
+                  ? 'Horizontal'
+                  : mostrarSubModalMisto
+                  ? 'Misto'
+                  : 'Layout da Página'}
+              </h3>
               <div className="linha-sublinhada"></div>
 
-              {!mostrarSubModal ? (
+              {!mostrarSubModalColunas && !mostrarSubModalHorizontal && !mostrarSubModalMisto && (
                 <>
-                  <button className="botao-modal" onClick={() => setMostrarSubModal(true)}>Colunas</button>
-                  <button className="botao-modal" onClick={() => alert('Horizontal')}>Horizontal</button>
-                  <button className="botao-modal" onClick={() => alert('Misto')}>Misto</button>
+                  <button className="botao-modal" onClick={() => setMostrarSubModalColunas(true)}>Colunas</button>
+                  <button className="botao-modal" onClick={() => setMostrarSubModalHorizontal(true)}>Horizontal</button>
+                  <button className="botao-modal" onClick={() => setMostrarSubModalMisto(true)}>Misto</button>
                 </>
-              ) : (
+              )}
+
+              {mostrarSubModalColunas && (
                 <div className="modal-colunas-conteudo">
                   <div className="linha-sublinhada2"></div>
                   <div className="div-cinza">
-                    <div 
-                      className={`opcao-layout ${layoutSelecionado === '1x1' ? 'selecionado' : ''}`} 
-                      onClick={() => handleLayoutClick('1x1')}
-                    >
+                    <div className={`opcao-layout ${layoutSelecionado === '1x1' ? 'selecionado' : ''}`} onClick={() => handleLayoutClick('1x1')}>
                       <div className="layout-visual">
                         <div className="caixa"></div>
                       </div>
                     </div>
-                    <div 
-                      className={`opcao-layout ${layoutSelecionado === '1x2' ? 'selecionado' : ''}`} 
-                      onClick={() => handleLayoutClick('1x2')}
-                    >
+                    <div className={`opcao-layout ${layoutSelecionado === '1x2' ? 'selecionado' : ''}`} onClick={() => handleLayoutClick('1x2')}>
                       <div className="layout-visual">
                         <div className="caixa"></div>
                         <div className="caixa"></div>
                       </div>
                     </div>
-                    <div 
-                      className={`opcao-layout ${layoutSelecionado === '1x3' ? 'selecionado' : ''}`} 
-                      onClick={() => handleLayoutClick('1x3')}
-                    >
+                    <div className={`opcao-layout ${layoutSelecionado === '1x3' ? 'selecionado' : ''}`} onClick={() => handleLayoutClick('1x3')}>
                       <div className="layout-visual">
                         <div className="caixa"></div>
                         <div className="caixa"></div>
@@ -83,7 +90,65 @@ const LayoutPage = () => {
                       </div>
                     </div>
                   </div>
+                  <div className="botoes">
+                    <button className="botao-voltar" onClick={voltarAoModalAnterior}>Voltar</button>
+                    <button className="botao-adicionar" onClick={() => alert('Adicionar')}>Adicionar</button>
+                  </div>
+                </div>
+              )}
 
+              {mostrarSubModalHorizontal && (
+                <div className="modal-colunas-conteudo">
+                  <div className="linha-sublinhada2"></div>
+                  <div className="div-cinza">
+                    <div className={`opcao-layout ${layoutSelecionado === 'h-1x1' ? 'selecionado' : ''}`} onClick={() => handleLayoutClick('h-1x1')}>
+                      <div className="layout-visual-horizontal">
+                        <div className="caixa-horizontal"></div>
+                      </div>
+                    </div>
+                    <div className={`opcao-layout ${layoutSelecionado === 'h-1x2' ? 'selecionado' : ''}`} onClick={() => handleLayoutClick('h-1x2')}>
+                      <div className="layout-visual-horizontal">
+                        <div className="caixa-horizontal"></div>
+                        <div className="caixa-horizontal"></div>
+                      </div>
+                    </div>
+                    <div className={`opcao-layout ${layoutSelecionado === 'h-1x3' ? 'selecionado' : ''}`} onClick={() => handleLayoutClick('h-1x3')}>
+                      <div className="layout-visual-horizontal">
+                        <div className="caixa-horizontal"></div>
+                        <div className="caixa-horizontal"></div>
+                        <div className="caixa-horizontal"></div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="botoes">
+                    <button className="botao-voltar" onClick={voltarAoModalAnterior}>Voltar</button>
+                    <button className="botao-adicionar" onClick={() => alert('Adicionar')}>Adicionar</button>
+                  </div>
+                </div>
+              )}
+
+              {mostrarSubModalMisto && (
+                <div className="modal-colunas-conteudo">
+                  <div className="linha-sublinhada2"></div>
+                  <div className="div-cinza2">
+                    <div className={`opcao-layout ${layoutSelecionado === 'm-1' ? 'selecionado' : ''}`} onClick={() => handleLayoutClick('m-1')}>
+                      <div className="layout-visual-misto">
+                        <div className="caixav-misto"></div>
+                        <div className="coluna">
+                          <div className="caixah-misto"></div>
+                          <div className="caixah-misto"></div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className={`opcao-layout ${layoutSelecionado === 'm-2' ? 'selecionado' : ''}`} onClick={() => handleLayoutClick('m-2')}>
+                      <div className="layout-visual-misto2">
+                        <div className="caixah-misto"></div>
+                        <div className="caixah-misto"></div>
+                        <div className="caixah-misto"></div>
+                        <div className="caixah-misto"></div>
+                      </div>
+                    </div>
+                  </div>
                   <div className="botoes">
                     <button className="botao-voltar" onClick={voltarAoModalAnterior}>Voltar</button>
                     <button className="botao-adicionar" onClick={() => alert('Adicionar')}>Adicionar</button>
